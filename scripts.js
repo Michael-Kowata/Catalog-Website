a376a39a99c44fcb8c7756d17d67a3a8c2e9dfbc
Michael Kowata <michael_kowata@elcamino.edu>
2024-04-08 00:35:53 -0700

    Update Catalog Project

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Data Catalog Project Starter Code - SEA Stage 2
  *
  * This file is where you should be doing most of your work. You should
@@ -23,16 +23,198 @@
  * 
  */
 
+//Make a snack class
+class Snacks {
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+    //Constructor
+    constructor(name, price, type){
+    this.name = name;
+    this.price = price;
+    this.type = type;
+    }
+
+    //Getters
+    getName()
+    {
+        return this.name;
+    }
+    
+    getPrice()
+    {
+        return this.price;
+    }
+
+    getType()
+    {
+        return this.type;
+    }
+
+    //Setters
+    setName(newName)
+    {
+        this.name = newName
+    }
+
+    setPrice(newPrice)
+    {
+        this.price = newPrice;
+    }
+
+    setType(newType)
+    {
+        this.type = newType;
+    }
+}
+
+let Inventory = [{ name :"Hot Cheetos", price : 1.50, type: "Spicy"}, { name :"Takis", price : 1.50, type: "Spicy"},
+{ name :"Nerds", price : 1.75, type: "Sweet"}, { name :"Starburst", price : 2.00, type: "Sweet"}, 
+{ name :"Pringles", price : 2.25, type: "Salty"}, { name :"Pretzel", price : 1.00, type: "Salty"}
+]
+
+let Reccomendation = new Set();
+
+function makeReccomendation(type){
+    valueFound = false;
+    for (let i = 0; i < Inventory.length; i++) {
+        if (type === Inventory[i].type) {
+            Reccomendation.add(Inventory[i]);
+            valueFound = true;
+        }
+    }
+
+    if (valueFound == false)
+    {
+        alert("Reccomendation Not Found!");
+    }
+    showAll();
+}
+
+function showReccomendationConsole(){
+    console.log("Reccomendation List: ")
+    Reccomendation.forEach(function(value) {
+        console.log(value);
+    });
+}
+
+function displayReccomendation(){
+    let reccomendationDiv = document.getElementById('Reccomendations');
+    
+    reccomendationDiv.innerHTML = '';
+
+    Reccomendation.forEach(function(snack) {
+        let snackDiv = document.createElement('div');
+        snackDiv.innerHTML = "[ " + snack.type + ", " + snack.name + ", $" + snack.price + "], ";
+
+        reccomendationDiv.appendChild(snackDiv);
+    });
+}
+
+function showInventoryConsole()
+{   console.log("Inventory List: ")
+    for (i in Inventory){
+        console.log(Inventory[i]);
+    }
+}
+
+function displayInventory()
+{
+    let catalogDiv = document.getElementById('snackInventory');
+    
+    catalogDiv.innerHTML = '';
+
+    Inventory.forEach(function(snack) {
+        let snackDiv = document.createElement('div');
+        snackDiv.innerHTML = "[ " + snack.type + ", " + snack.name + ", $" + snack.price + "], ";
+
+        catalogDiv.appendChild(snackDiv);
+    });
+}
+
+function addSnack()
+{
+    let snackName = document.getElementById('snackName').value;
+    let snackPrice = document.getElementById('snackPrice').value;
+    let snackType = document.getElementById('snackType').value;
+
+    let snack = {
+    name: snackName,
+    price: Number(snackPrice),
+    type: snackType
+    };
+    Inventory.push(snack);
+    showAll();
+}
+
+function deleteSnack(name) {
+    for (let i = 0; i < Inventory.length; i++) {
+        if (name == Inventory[i].name) {
+            Inventory.splice(i, 1);          
+            console.log("Deleted: " + name);
+            showAll();
+            return; 
+        }
+    }
+    alert("Snack Not Found!");
+    console.log("Snack not found: " + name);  
+}
+
+function clearReccomendation()
+{
+    Reccomendation.clear();
+    showAll();
+}
+
+function clearInventory()
+{
+    Inventory.splice(0,Inventory.length);
+    showAll();
+}
+
+function calculateReccomendation()
+{   
+    let rectotal = 0;
+    Reccomendation.forEach(function(value) {
+        rectotal =  rectotal + value.price;
+    });
+    console.log("Total Reccommedation Database Value: $" + rectotal);
+    
+    let catalogDiv = document.getElementById('recTotal');
+    catalogDiv.textContent = "$ " + rectotal;
+}
+
+function calculateInventory()
+{   
+    let intotal = 0;
+    Inventory.forEach(function(value) {
+        intotal =  intotal + value.price;
+    });
+    console.log("Total Inventory Database Value: $" + intotal);
+
+    let catalogDiv = document.getElementById('inTotal');
+    catalogDiv.textContent = '';
+    catalogDiv.textContent = "$ " + intotal;
+}
+
+function showAll()
+{
+    showReccomendationConsole();  
+    showInventoryConsole();
+    displayReccomendation();
+    displayInventory();
+    calculateReccomendation();
+    calculateInventory(); 
+}
+
+/*
+const SPICY_URL = "https://m.media-amazon.com/images/I/71lwqLV0CLL.jpg";
+const SWEET_URL = "https://m.media-amazon.com/images/I/91mKILVPzDL.jpg";
+const SALTY_URL = "https://hips.hearstapps.com/hmg-prod/images/salty-snacks-6516f9a2f3702.jpg?crop=0.495xw:0.990xh;0,0.00962xh&resize=640:*";
 
 // This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
+let types = [
+    "Spicy",
+    "Sweet",
+    "Salty",
 ];
 // Your final submission should have much more data than this, and 
 // you should use more than just an array of strings to store it all.
@@ -40,22 +222,22 @@ let titles = [
 
 // This function adds cards the page to display the data in the array
 function showCards() {
-    const cardContainer = document.getElementById("card-container");
+    const cardContainer = document.getElementById("snack-container");
     cardContainer.innerHTML = "";
     const templateCard = document.querySelector(".card");
     
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
+    for (let i = 0; i < types.length; i++) {
+        let title = types[i];
 
         // This part of the code doesn't scale very well! After you add your
         // own data, you'll need to do something totally different here.
         let imageURL = "";
         if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
+            imageURL = SPICY_URL;
         } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
+            imageURL = SWEET_URL;
         } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
+            imageURL = SALTY_URL;
         }
 
         const nextCard = templateCard.cloneNode(true); // Copy the template card
@@ -89,6 +271,7 @@ function quoteAlert() {
 }
 
 function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
+    types.pop(); // Remove last item in titles array
     showCards(); // Call showCards again to refresh
 }
+*/
